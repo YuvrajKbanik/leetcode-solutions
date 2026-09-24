78cd1191136fdc7e1361be88aa1937760d057303
Yuvraj Kansa banik <yuvrajkbanik@gmail.com>
2026-09-24 12:17:13 +0530

    Time: 4 ms (20.63%), Space: 44.2 MB (8.32%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+    
+       
+       String res="";
+        for(int i =0 ; i<Math.max(word1.length(),word2.length());i++)
+        {
+            if(i<word1.length())
+            res+=word1.charAt(i);
+            if(i<word2.length())
+            res+=word2.charAt(i);
+            
+        }
+    return res;      
+    }
+}
